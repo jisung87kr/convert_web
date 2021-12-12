85fb8428cd16235de2380158500080fe1288b015
jisungyoo <jiusng87kr@gmail.com>
2021-12-13 00:21:42 +0900

    퍼블리싱 수정

--- public_html/app/resources/views/convert/image.blade.php
+++ public_html/app/resources/views/convert/image.blade.php
@@ -5,7 +5,11 @@
     <h1>{{$data['title']}}</h1>
     <form action="{{route('convert.process', $data['type'])}}" class="row" method="POST" target="_blank" enctype="multipart/form-data">
         @csrf
-        <div class="col-3">
+        <div class="col-7">
+            <input type="file" class="form-control" name="source" placeholder="http://example.com">
+        </div>
+        <div class="col-1 text-center"> 파일을 </div>
+        <div class="col-2">
             <select name="ext" id="" class="form-select">
                 <option value="jpg">jpg</option>
                 <option value="png">png</option>
@@ -13,11 +17,8 @@
                 <option value="tiff">tiff</option>
             </select>
         </div>
-        <div class="col-7">
-            <input type="file" class="form-control" name="source" placeholder="http://example.com">
-        </div>
         <div class="col-2">
-            <input type="submit" class="btn btn-primary">
+            <input type="submit" class="btn btn-primary" name="변환하기">
         </div>
     </form>
 </div>
